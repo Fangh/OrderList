f9752cddbcead55121ecc8175afdc3962ab5679d
Morgan Klein - 6freedom <fangh.klein@gmail.com>
2023-06-18 11:01:43 +0200

    Update product list

--- src/ProductList.js
+++ src/ProductList.js
@@ -9,24 +9,29 @@ class ProductList extends React.Component
         super(props);
         let productList = [];
         productList.push(new Product("Adhésion à l'asso", 5));
-        productList.push(new Product("Bière Blonde 25cl", 2.5));
-        productList.push(new Product("Bière Blonde 50cl", 4.5));
-        productList.push(new Product("Bière Bouteille", 3.5));
-        productList.push(new Product("Bière Titan IPA 25cl", 3));
-        productList.push(new Product("Bière Titan IPA 50cl", 5.5));
-        productList.push(new Product("Bol Repas", 6));
-        productList.push(new Product("Café court/allongé", 2));
+        productList.push(new Product("Pression Orge bio 25cl", 2.5));
+        productList.push(new Product("Pression Orge bio 50cl", 4.5));
+        productList.push(new Product("Pression Titan IPA 25cl", 3));
+        productList.push(new Product("Pression Titan IPA 50cl", 5.5));
+        productList.push(new Product("Bière Blanche Bouteille 33cl", 3.5));
+        productList.push(new Product("Bière Ambrée Bouteille 33cl", 3.5));
+        productList.push(new Product("Vin blanc 12.5cl", 2.5));
+        productList.push(new Product("Vin rouge 12.5cl", 2.5));
+        productList.push(new Product("Cidre Brut 25cl", 3));
+        productList.push(new Product("Jus de fruit bio 25cl", 2));
+        productList.push(new Product("Jus de fruit bouteille 25cl", 2));
+        productList.push(new Product("Bière sans alcool 0% 25cl", 2.5));
+        productList.push(new Product("Sirop & Citronnade 25cl", 1));
+        productList.push(new Product("Limonade & Diabolo 25cl", 2));
+        productList.push(new Product("Breizh Cola 25cl", 2));
+        productList.push(new Product("Infusion glacée 33cl", 3));
+        productList.push(new Product("Café court & allongé", 1));
         productList.push(new Product("Café double", 2));
         productList.push(new Product("Chocolat Chaud", 2));
-        productList.push(new Product("Eau Gazeuse", 1));
-        productList.push(new Product("Jus citronnée", 1));
-        productList.push(new Product("Jus de Fruit", 2));
-        productList.push(new Product("Verre de de Vin", 2.5));
-        productList.push(new Product("Sirop à l'eau", 1));
-        productList.push(new Product("Soupe", 3));
-        productList.push(new Product("Sodas", 2));
-        productList.push(new Product("Thé", 2));
-        productList.push(new Product("Planche tartinade", 5));
+        productList.push(new Product("Thés et infusions", 2));
+        productList.push(new Product("Bol Repas bio végé", 6));
+        productList.push(new Product("Planche apéro bio végan", 5));
+        productList.push(new Product("Soupe bio", 3));
 
         this.state =
         {
