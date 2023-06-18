ce76ba7f61228b4fcd9390ae9610c9e258ad0450
Morgan Klein - 6freedom <fangh.klein@gmail.com>
2023-06-18 11:08:20 +0200

    V1.4

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component
     this.nameModal = null;
     this.tableManager = new TableManager(this);
 
-    this.version = "V1.3";
+    this.version = "V1.4";
     console.log(this.version);
   }
 
